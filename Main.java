495f55cf76e8e085bb36c3120d75327a6a43a522
ethanparab <59212466+ethanparab@users.noreply.github.com>
2021-03-29 19:44:44 -0500

    added command framework

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 class Main {
+	public static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		Song[] songs = {
 				new Song("When Am I Going to Lose You", "Local Natives", 4, 24),
@@ -10,7 +14,31 @@ class Main {
 		PlayList playList = new PlayList(songs.length);
 		playList.playList = songs;
 
-		playList.sortAlpha();
-		System.out.println(playList);
+		System.out.println("Create your own playlist! Type \"init\" to begin ");
+		while(true) {
+			String cmd = input("> ");
+			if(cmd.startsWith("init")) {
+				;
+			} else if (cmd.startsWith("remove")) {
+				;
+			} else if (cmd.startsWith("sort")) {
+				if (cmd.substring(5).startsWith("-d")) {
+					playList.sortDuration();
+				} else if (cmd.substring(5).startsWith("-a")) {
+					playList.sortAlpha();
+				} else {
+					; //TODO add a help message for sort
+				}
+			} else if (cmd.startsWith("get")) {
+				System.out.println(playList.getSong(Integer.parseInt(cmd.substring(4))));
+			} else if (cmd.startsWith("print")) {
+				System.out.println(playList);
+			}
+		}
+	}
+
+	public static String input(String prompt) {
+		System.out.print(prompt);
+		return input.nextLine();
 	}
 }
