bb30f7ac987770a54b91d72b6a3bcb19076407c9
ethanparab <59212466+ethanparab@users.noreply.github.com>
2021-03-30 09:30:22 -0500

    added input

--- Main.java
+++ Main.java
@@ -11,16 +11,27 @@ class Main {
 				new Song("Out of My League", "Fitz & The Tantrums", 3, 29)
 		};
 
-		PlayList playList = new PlayList(songs.length);
-		playList.playList = songs;
+		System.out.println("Create your own playlist!");
 
-		System.out.println("Create your own playlist! Type \"init\" to begin ");
+		PlayList playList = makePlaylist();
+
+		System.out.println("~~BEGIN DEMO~~");
+		System.out.println("\nSorted alphabetically and printed:");
+		playList.sortAlpha();
+		System.out.println(playList);
+		System.out.println("\nSorted by duration and printed:");
+		playList.sortDuration();
+		System.out.println(playList);
+
+		System.out.println("~~ END DEMO~~");
+
+		help();
 		while(true) {
 			String cmd = input("> ") + "                 "; // the extra spaces are a quick fix to avoid index issues in some of the if-statements
 			if(cmd.startsWith("init")) {
-				; //TODO add input system
+				playList = makePlaylist();
 			} else if (cmd.startsWith("remove")) {
-				playList.remove(Integer.parseInt(cmd.trim().substring(7)));
+				playList.remove(Integer.parseInt(cmd.trim().substring(7))-1); //TODO add removal try-catch
 			} else if (cmd.startsWith("sort")) {
 				if (cmd.substring(5).startsWith("-d")) {
 					playList.sortDuration();
@@ -30,13 +41,70 @@ class Main {
 					System.out.println("-d\tSort by duration\n-a\tSort alphabetically");
 				}
 			} else if (cmd.startsWith("get")) {
-				System.out.println(playList.getSong(Integer.parseInt(cmd.substring(4))));
+				try {
+					System.out.println(playList.getSong(Integer.parseInt(cmd.trim().substring(4))-1));
+				} catch (IllegalArgumentException e) {
+					System.out.println("Please follow the get command with a space and a non-negative integer that is a valid position to retrieve.");
+				}
 			} else if (cmd.startsWith("print")) {
 				System.out.println(playList);
+			} else if (cmd.startsWith("help")) {
+				help();
 			} else if (cmd.startsWith("exit")) {
 				System.exit(0);
 			}
 		}
+	}
+
+	public static void help() {
+		System.out.println("In order to do things, type one of the following commands:\n" +
+				"init\t-will allow you to make a new playlist from scratch\n" +
+				"exit\t-will exit the program\n" +
+				"sort\t-will sort the playlist (follow with space and either \"-a\" (for alphabetical) or \"-d\" (for duration-based)\n" +
+				"get\t\t-will get an element from the playlist, follow with space and the index to retrieve (starts counting at 1)\n" +
+				"remove\t-will remove an element from the playlist, follow with space and the index to remove (starts counting at 1)\n" +
+				"print\t-will print all elements of the playlist (will print positions, which start at 1 and are one higher than indices)\n");
+	}
+
+	public static PlayList makePlaylist() {
+		int num = 0;    // number of songs
+		while (true) {
+			try {
+				num = Integer.parseInt(input("How many songs would you like in your playlist?\n"));
+				if(num < 0) {
+					throw new NumberFormatException();
+				}
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Please enter a non-negative integer. Try again.");
+			}
+		}
+
+		PlayList tempList = new PlayList(num);
+
+		for(int i = 0; i < num; i++) {
+			String name = input("Enter the name of song " + (i+1) + ": ");
+			String artist = input("Enter the artist of song " + (i+1) + ": ");
+			int min;
+			int sec;
+
+			while (true) {
+				try {
+					min = Integer.parseInt(input("Enter the minutes of song " + (i + 1) + ": "));
+					sec = Integer.parseInt(input("Enter the seconds of song " + (i + 1) + ": "));
+					break;
+				} catch (NumberFormatException e) {
+					System.out.println("Please enter non-negative integers. Try again.");
+				}
+			}
+
+			tempList.setSong(i, new Song(name, artist, min, sec));
+
+			System.out.println();
+		}
+
+		System.out.println("Playlist creation complete.");
+		return tempList;
 	}
 
 	public static String input(String prompt) {
